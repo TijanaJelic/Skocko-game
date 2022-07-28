c249d1b0b53d27901c1a4545ae5dcc34b41515cd
TijanaJelic <tijanajelic.jelic@gmail.com>
2022-07-28 17:41:56 +0200

    Setting up a new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StartGame from './components/StartGame/StartGame';
+import Game from './components/Game/Game';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StartGame />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/StartGame/StartGame.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const StartGame = () => {
+  return (
+    <div>
+      <Link to={'game'}>Start Game</Link>
+    </div>
+  );
+};
+
+export default StartGame;
